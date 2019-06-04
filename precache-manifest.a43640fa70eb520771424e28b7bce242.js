9a1c3d90102e5bf2649792f1c27dd4bdcfeba407
Warner Carpenter <warnercarpenter@yahoo.com>
2019-06-04 12:50:50 -0500

    Updates

---
+++ precache-manifest.a43640fa70eb520771424e28b7bce242.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543167ba0341da26d8dd3aac813e8db4",
+    "url": "/class-website/index.html"
+  },
+  {
+    "revision": "16817f11d96a1d472379",
+    "url": "/class-website/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "efb20569ec6a1a3d74c8",
+    "url": "/class-website/static/css/main.d125424f.chunk.css"
+  },
+  {
+    "revision": "16817f11d96a1d472379",
+    "url": "/class-website/static/js/2.2b2ce545.chunk.js"
+  },
+  {
+    "revision": "efb20569ec6a1a3d74c8",
+    "url": "/class-website/static/js/main.b5c065da.chunk.js"
+  },
+  {
+    "revision": "81a1cbd47083f2270dc5",
+    "url": "/class-website/static/js/runtime~main.247608a7.js"
+  },
+  {
+    "revision": "b6d1469729aa29294949ebc3d422e0d9",
+    "url": "/class-website/static/media/AbbeyBrown.b6d14697.jpg"
+  },
+  {
+    "revision": "38459efd4b367dda24fbaaa3187db6e5",
+    "url": "/class-website/static/media/AbbeyBrown_Fun.38459efd.jpg"
+  },
+  {
+    "revision": "7ee71259051efca6c4cd2d56d933658a",
+    "url": "/class-website/static/media/AntonioJefferson.7ee71259.jpg"
+  },
+  {
+    "revision": "7eccfa7d8a70cc7bcf33b40b21531275",
+    "url": "/class-website/static/media/AntonioJefferson_Fun.7eccfa7d.jpg"
+  },
+  {
+    "revision": "3eab34085bebd881db49a28389e105a4",
+    "url": "/class-website/static/media/BrianNeal.3eab3408.jpg"
+  },
+  {
+    "revision": "f4ec768cdfcd529430b74a05d3201ea6",
+    "url": "/class-website/static/media/BrianNeal_Fun.f4ec768c.jpg"
+  },
+  {
+    "revision": "0cf9229d420bd843a9e8a9d0a7463ea5",
+    "url": "/class-website/static/media/CharlesWoodring.0cf9229d.jpg"
+  },
+  {
+    "revision": "725beeb75bfb6615b11be95a15bc2363",
+    "url": "/class-website/static/media/CharlesWoodring_Fun.725beeb7.jpg"
+  },
+  {
+    "revision": "74939d7fa3044a8b8c285d0a74c366fc",
+    "url": "/class-website/static/media/ConnorBailey.74939d7f.jpg"
+  },
+  {
+    "revision": "416e8e7dd56063143934c51a7cabd0f9",
+    "url": "/class-website/static/media/ConnorBailey_Fun.416e8e7d.jpg"
+  },
+  {
+    "revision": "8f961f46c4ade18d6b2ac0b114055d53",
+    "url": "/class-website/static/media/DominicKantrude.8f961f46.jpg"
+  },
+  {
+    "revision": "01ddc201175ed8ab7729d075babf69a6",
+    "url": "/class-website/static/media/DominicKantrude_Fun.01ddc201.jpg"
+  },
+  {
+    "revision": "8c2edfd20218dd193412d5541a568f82",
+    "url": "/class-website/static/media/EmmettSchneider.8c2edfd2.jpg"
+  },
+  {
+    "revision": "84ff7fa7b744e789ba1360c0d3c31a7c",
+    "url": "/class-website/static/media/EmmettSchneider_Fun.84ff7fa7.jpg"
+  },
+  {
+    "revision": "cdbd542dda8f688b97f9ff4c19abc578",
+    "url": "/class-website/static/media/JacobSanders.cdbd542d.jpg"
+  },
+  {
+    "revision": "d5e08c4c010916007d60d2c664a4dfc7",
+    "url": "/class-website/static/media/JacobSanders_Fun.d5e08c4c.jpg"
+  },
+  {
+    "revision": "a4f6b295162c25253156b221fa1d4b83",
+    "url": "/class-website/static/media/JamesFarrell.a4f6b295.jpg"
+  },
+  {
+    "revision": "ec0243458e79e93a28ad2f482dcac380",
+    "url": "/class-website/static/media/JamesFarrell_Fun.ec024345.jpg"
+  },
+  {
+    "revision": "5fb38f5e4660f4a106b66b0bce388099",
+    "url": "/class-website/static/media/JanetWoods.5fb38f5e.jpg"
+  },
+  {
+    "revision": "e1a95db04217fd6b0d5a198fe93c6d03",
+    "url": "/class-website/static/media/JanetWoods_Fun.e1a95db0.jpg"
+  },
+  {
+    "revision": "cecdd46fea1f050e35a6a86d2d081f2d",
+    "url": "/class-website/static/media/JustinaVickers.cecdd46f.jpg"
+  },
+  {
+    "revision": "971d804fa084c00ee530b0b2e8e9eb4b",
+    "url": "/class-website/static/media/JustinaVickers_Fun.971d804f.jpg"
+  },
+  {
+    "revision": "01cf8e2f7e5645ee3b68524c4b58b99f",
+    "url": "/class-website/static/media/KaterinaFreeman.01cf8e2f.jpg"
+  },
+  {
+    "revision": "fa42fa92effe6a54d60905707fa9ebe2",
+    "url": "/class-website/static/media/KaterinaFreeman_Fun.fa42fa92.jpg"
+  },
+  {
+    "revision": "eb124fa42a9b919c9a0fb3bf368aff75",
+    "url": "/class-website/static/media/KenPerkerwicz.eb124fa4.jpg"
+  },
+  {
+    "revision": "73b4e9979b722be6b8d511b422741c6b",
+    "url": "/class-website/static/media/KenPerkerwicz_Fun.73b4e997.jpg"
+  },
+  {
+    "revision": "906c10d4c89536b1139ee8f9a0ea5e31",
+    "url": "/class-website/static/media/KirrenCovey.906c10d4.jpg"
+  },
+  {
+    "revision": "548dd77da7f7481ff922ac1a5a5cc2db",
+    "url": "/class-website/static/media/KirrenCovey_Fun.548dd77d.jpg"
+  },
+  {
+    "revision": "a02acba4120468a80ae1db0bc5f64734",
+    "url": "/class-website/static/media/MoSilvera.a02acba4.jpg"
+  },
+  {
+    "revision": "05455776294f63a97aad203abac8e698",
+    "url": "/class-website/static/media/MoSilvera_Fun.05455776.jpg"
+  },
+  {
+    "revision": "56f8da510b915927bbcba4bb9ac928fb",
+    "url": "/class-website/static/media/NiallFraser.56f8da51.jpg"
+  },
+  {
+    "revision": "e4ad05380c7d943b126ed35d814f7732",
+    "url": "/class-website/static/media/NiallFraser_Fun.e4ad0538.jpg"
+  },
+  {
+    "revision": "088fcac0909363e03128bf54f80c153d",
+    "url": "/class-website/static/media/PanyaFarnette.088fcac0.jpg"
+  },
+  {
+    "revision": "e641240d06c1be9d84297eb04b2d63e8",
+    "url": "/class-website/static/media/PanyaFarnette_Fun.e641240d.jpg"
+  },
+  {
+    "revision": "78073bec3a64992eb3ada53b6d983fc2",
+    "url": "/class-website/static/media/PrafullataSonawane.78073bec.jpg"
+  },
+  {
+    "revision": "de05b7b807f98088775855fe8b0ecee9",
+    "url": "/class-website/static/media/PrafullataSonawane_Fun.de05b7b8.jpg"
+  },
+  {
+    "revision": "285512d3591bb2a27640ed0422b8f444",
+    "url": "/class-website/static/media/RyanDillinger.285512d3.jpg"
+  },
+  {
+    "revision": "9646d371cf7bd287b2c3fa3aa397c037",
+    "url": "/class-website/static/media/RyanDillinger_Fun.9646d371.jpg"
+  },
+  {
+    "revision": "5ebecbc2fc92502c7faae9688cff8249",
+    "url": "/class-website/static/media/RyanNelson.5ebecbc2.jpg"
+  },
+  {
+    "revision": "b1a6512e98f1e256f62558557e6405f9",
+    "url": "/class-website/static/media/RyanNelson_Fun.b1a6512e.jpg"
+  },
+  {
+    "revision": "a8d9f9ac88117bfb71ac43c0e463b11b",
+    "url": "/class-website/static/media/SamuelCronin.a8d9f9ac.jpg"
+  },
+  {
+    "revision": "290210ecf1aff9a105fc3a32dd3a31ea",
+    "url": "/class-website/static/media/SamuelCronin_Fun.290210ec.jpg"
+  },
+  {
+    "revision": "b0690b667bef4030ad85432cef75a6ed",
+    "url": "/class-website/static/media/StephenClark.b0690b66.jpg"
+  },
+  {
+    "revision": "93a3d2ce06e4198da7fc097281d04085",
+    "url": "/class-website/static/media/StephenClark_Fun.93a3d2ce.jpg"
+  },
+  {
+    "revision": "7a097e3d8c5027edae899692f0f4749e",
+    "url": "/class-website/static/media/TrevanYoung.7a097e3d.jpg"
+  },
+  {
+    "revision": "0b390b9c9e72b2cb854132a500b63219",
+    "url": "/class-website/static/media/TrevanYoung_Fun.0b390b9c.jpg"
+  },
+  {
+    "revision": "ad6d3842db3a25de92491da135c96ec8",
+    "url": "/class-website/static/media/WarnerCarpenter.ad6d3842.jpg"
+  },
+  {
+    "revision": "f62f68ac8518745900488e2700322e51",
+    "url": "/class-website/static/media/WarnerCarpenter_Fun.f62f68ac.jpg"
+  },
+  {
+    "revision": "74f7cdffc9b2c3f62771c7acac3f512d",
+    "url": "/class-website/static/media/background.74f7cdff.png"
+  },
+  {
+    "revision": "ba60afcb86134350430a62670e4224b6",
+    "url": "/class-website/static/media/bootstrap.ba60afcb.svg"
+  },
+  {
+    "revision": "f00f4fb99fa5abebfbd57491fe81d64a",
+    "url": "/class-website/static/media/csharp.f00f4fb9.png"
+  },
+  {
+    "revision": "0abf0024aa7d86e2b1ad924d9a7603a6",
+    "url": "/class-website/static/media/css3.0abf0024.png"
+  },
+  {
+    "revision": "c1a2e8e623de9fab817d7cd5b9b70a96",
+    "url": "/class-website/static/media/dotnetmvc.c1a2e8e6.png"
+  },
+  {
+    "revision": "c95b7639d5554849e4ba9240e267f9bb",
+    "url": "/class-website/static/media/fun.c95b7639.jpg"
+  },
+  {
+    "revision": "7eab949cf687f89cff74744df16a0876",
+    "url": "/class-website/static/media/grunt.7eab949c.png"
+  },
+  {
+    "revision": "b012529ab0ac6954f6ae34038bb9ebf6",
+    "url": "/class-website/static/media/javascript.b012529a.svg"
+  },
+  {
+    "revision": "2622134f287b25785990ba67f257d69d",
+    "url": "/class-website/static/media/logo.2622134f.png"
+  },
+  {
+    "revision": "e03f64c51556e20245b3ffb515cbf938",
+    "url": "/class-website/static/media/serious.e03f64c5.jpg"
+  },
+  {
+    "revision": "8ddf4af95b50b9826ef29cb92ef6378f",
+    "url": "/class-website/static/media/sqlserver.8ddf4af9.png"
+  },
+  {
+    "revision": "4290261f6980e103d5851a38505d44fb",
+    "url": "/class-website/static/media/visualstudio.4290261f.png"
+  }
+]);
